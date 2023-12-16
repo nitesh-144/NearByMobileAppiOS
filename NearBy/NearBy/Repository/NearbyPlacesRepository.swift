4459ff7eaa580eb6fb1c60664dfbf81f776be430
Nitesh Vyas <niteshvyas144@gmail.com>
2023-12-16 18:36:51 +0530

    Repository setup

--- NearBy/NearBy/Repository/NearbyPlacesRepository.swift
+++ NearBy/NearBy/Repository/NearbyPlacesRepository.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
+enum RepositoryError: Error {
+    case placesNotFound
+    case somethingWentWrong
+    case networkIssue(errorMsg: String)
+    case dbError(errorMsg: String)
+}
+
+typealias NearByPlacesRepositoryCompletion = (Result<[NearByVenue],RepositoryError>) -> Void
+
+protocol NearByVenue {
+    var name: String { get set }
+    var address: String { get set }
+    var ticketURL: String { get set }
+}
+
 protocol NearByPlacesRepositoryProtocol {
-    
+    func getPlaces(forLatitude latitude: Double, forLongitude longitude: Double, pageNo: Int, completion: @escaping NearByPlacesRepositoryCompletion)
+}
+
+class NearbyRepository: NearByPlacesRepositoryProtocol {
+    func getPlaces(forLatitude latitude: Double, forLongitude longitude: Double, pageNo: Int, completion: @escaping NearByPlacesRepositoryCompletion) {
+        
+    }
 }
---
+++ NearBy/NearBy/Repository/NetworkService/NearByAppNetworkService.swift
@@ -0,0 +1,19 @@
+//
+//  NearByAppNetworkService.swift
+//  NearBy
+//
+//  Created by Deepika Vyas on 16/12/23.
+//
+
+import Foundation
+
+struct NetworkConfig {
+    
+}
+
+class NearbyAppNetworkService: NearByPlacesRepositoryProtocol {
+    
+    func getPlaces(forLatitude latitude: Double, forLongitude longitude: Double, pageNo: Int, completion: @escaping NearByPlacesRepositoryCompletion) {
+        
+    }
+}
